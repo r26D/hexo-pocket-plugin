1b9205c0a853d7132573c187f74e58d8faa5aad9
Dirk Elmendorf <dirk@economysizegeek.com>
2017-04-27 17:29:24 -0500

    Working on getting carvbed up

---
+++ README.md
@@ -0,0 +1,29 @@
+#  Hexo Pocket Plugin
+
+I use [Pocket](https://getpocket.com/) to save and read articles.  Pocket 
+allows you to recommend an article to people who follow you. I wanted to be able
+to recommend articles in Pocket and have them show up as "Recommended Reading" in
+my blog. 
+
+This plugin can be configured to parse the public recommendation feed for a user. It will 
+automatically create a blog post for that recommendation if one does not already exist.
+
+## How to Configure
+```
+pocket:
+  enable: true
+  feeds:
+    - username:
+      author_name:
+      layout:
+    - username:
+      author_name:
+      layout:
+```
+
+
+## Special Shout Out
+
+I have to give special thanks to [Hexo Instagram Wall](https://github.com/rdgpt/hexo-instagram-wall) plugin.
+This is the first hexo plugin I've ever used, and the Instagram plugin served as a great guide
+on how to make this work.
--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-#!/usr/bin/node
-
-var osmosis = require('osmosis');
-var moment = require('moment');
-
+#!/usr/bin/env node
+"use strict";
+const lodash = require("lodash");
+const fetchFeeds = require('./lib/fetchFeeds');
+const logger = require('hexo-log')({
+  debug: true,
+  silent: false
+});
 //https://getpocket.com/@belmendo
 //https://getpocket.com/@economysizegeek
 //https://getpocket.com/@harperreed
-osmosis
-.get('https://getpocket.com/@harperreed')
-.find("div.sprofile-post")
-.set( {
-  'img': ".sprofile-article-img @data-bgimg",
-  'link': '.sprofile-article-link @href',
-  'title': ".sprofile-article-title",
-  'comment': ".sprofile-attribution-comment",
-  'posted_on': ".sprofile-post-time"
-})
-.data(function(data) {
-   var num_pattern =/\d+/;
-   var number = data.posted_on.match(num_pattern)[0];
-   console.log("Number is",number);
-    if (data.posted_on.match("hour")) {
-      data.posted_date = moment().subtract(number, "hours");
-    }
-    else {
-      data.posted_date = moment().subtract(number, "days");
-    }
 
-       console.log(data);      // Data here would be each search result with the properties that we set above
-})
-.log(console.log)
-.error(console.log)
-.debug(console.log)
+var config = {};
+config.pocket = lodash.assign(
+    {},
+    {
+        enable: true,
+        feeds: [
+            {
+                username: "belmendo",
+                author_name: "Brett Elmendorf"
+            },
+            {
+                username: "economysizegeek",
+                author_name: "Dirk Elmendorf"
+            },
+            {
+                username: "harperread",
+                author_name: "Harper Reed"
+            }
+        ]
+    }
+);
+
+if (config.pocket.enable) {
+    fetchFeeds(config, logger);
+    // hexo.extend.filter.register('after_init', function() {
+    //
+    //   return fetchFeeds(config, logger);
+    // });
+    // hexo.extend.filter.register('after_render:html', function(str, data) {
+    //
+    //
+    //   return generateAssets(str, data, config, logger);
+    // });
+
+}
