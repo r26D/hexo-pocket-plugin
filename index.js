9cca296bb0669c6483e31deafe10f4345e22fe90
Dirk Elmendorf <dirk@economysizegeek.com>
2017-04-27 22:23:28 -0500

    Completed fetch and simple gneration with caching. Now need to make it work as a plugin

--- index.js
+++ index.js
@@ -3,45 +3,26 @@
 const lodash = require("lodash");
 const fetchFeeds = require('./lib/fetchFeeds');
 const logger = require('hexo-log')({
-  debug: true,
-  silent: false
+    debug: true,
+    silent: false
 });
-//https://getpocket.com/@belmendo
-//https://getpocket.com/@economysizegeek
-//https://getpocket.com/@harperreed
 
-var config = {};
+const config = hexo.config;
+
 config.pocket = lodash.assign(
     {},
     {
-        enable: true,
-        feeds: [
-            {
-                username: "belmendo",
-                author_name: "Brett Elmendorf"
-            },
-            {
-                username: "economysizegeek",
-                author_name: "Dirk Elmendorf"
-            },
-            {
-                username: "harperread",
-                author_name: "Harper Reed"
-            }
-        ]
-    }
+        enabled: false,
+        feeds: []
+    },
+    config.pocket
+
 );
 
 if (config.pocket.enable) {
     fetchFeeds(config, logger);
-    // hexo.extend.filter.register('after_init', function() {
-    //
-    //   return fetchFeeds(config, logger);
-    // });
-    // hexo.extend.filter.register('after_render:html', function(str, data) {
-    //
-    //
-    //   return generateAssets(str, data, config, logger);
-    // });
+    hexo.extend.filter.register('after_init', function () {
 
+        return fetchFeeds(config, logger);
+    });
 }
